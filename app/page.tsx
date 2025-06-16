4074f0a4546eca4439d5c82ab473ba6bb50c1450
Epov Nikita <Nikita.Epov@develonica.ru>
2025-06-16 13:47:20 +0800

    сделал разметку главной страницы

---
+++ app/page.tsx
@@ -0,0 +1,43 @@
+'use client';
+
+import {Card} from "@/components";
+
+export default function HomePage() {
+    return (
+       <>
+           <Card
+               category="Front-end"
+               point="·"
+               date="1 месяц назад"
+               title="Как работать с CSS Grid"
+               description="Грид-раскладка (CSS Grid Layout) представляет собой двумерную систему сеток в CSS. Гриды подойдут и для верстки основных областей страницы.."
+               readTime="3 минуты"
+               onReadClick={() => {}}
+               likeCount={4}
+               isLiked={false}
+           />
+           <Card
+               category="Front-end"
+               point="·"
+               date="1 месяц назад"
+               title="Как работать с CSS Grid"
+               description="Грид-раскладка (CSS Grid Layout) представляет собой двумерную систему сеток в CSS. Гриды подойдут и для верстки основных областей страницы.."
+               readTime="3 минуты"
+               onReadClick={() => {}}
+               likeCount={4}
+               isLiked={false}
+           />
+           <Card
+               category="Front-end"
+               point="·"
+               date="1 месяц назад"
+               title="Как работать с CSS Grid"
+               description="Грид-раскладка (CSS Grid Layout) представляет собой двумерную систему сеток в CSS. Гриды подойдут и для верстки основных областей страницы.."
+               readTime="3 минуты"
+               onReadClick={() => {}}
+               likeCount={4}
+               isLiked={false}
+           />
+       </>
+    );
+}
